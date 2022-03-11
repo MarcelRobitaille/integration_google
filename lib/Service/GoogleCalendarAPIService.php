906ac347905f1aa652c0a5312a6319ddd84c37c7
Marcel Robitaille <mail@marcelrobitaille.me>
2022-03-11 09:42:53 -0500

    Use lockfile to ensure calendar fully imported
    Prevent two imports from running on the same calendar at the same time,
    which can cause big problems.
    
    Signed-off-by: Marcel Robitaille <mail@marcelrobitaille.me>

--- lib/Service/GoogleCalendarAPIService.php
+++ lib/Service/GoogleCalendarAPIService.php
@@ -151,155 +151,172 @@ class GoogleCalendarAPIService {
 	 * @return array
 	 */
 	public function importCalendar(string $accessToken, string $userId, string $calId, string $calName, ?string $color = null): array {
-		$params = [];
-		if ($color) {
-			$params['{http://apple.com/ns/ical/}calendar-color'] = $color;
+
+		$lockFile = sys_get_temp_dir() .
+			"/nextcloud_integration_google_calendar_import_$calId.lock";
+
+		if (file_exists($lockFile)) {
+			throw new Exception('Could not acquire lock');
 		}
 
-		$newCalName = trim($calName) . ' (' . $this->l10n->t('Google Calendar import') .')';
-		$ncCalId = $this->calendarExists($userId, $newCalName);
-		if (is_null($ncCalId)) {
-			$ncCalId = $this->caldavBackend->createCalendar('principals/users/' . $userId, $newCalName, $params);
-		}
+		touch($lockFile);
 
-		// Delete old calendar events
-		// Only trust new ones
-		// They could have been deleted in Google Calendar or moved,
-		// in which case they would be doubled
-		// TODO: This is not very efficient, really we should check what's in Google
-		// Calendar and only delete the stuff that was deleted there
-		foreach ($this->caldavBackend->getCalendarObjects($ncCalId) as $e) {
-			$this->caldavBackend->deleteCalendarObject($ncCalId, $e['uri'], $this->caldavBackend::CALENDAR_TYPE_CALENDAR, true);
-		}
-
-		// get color list
-		$eventColors = [];
-		$colors = $this->googleApiService->request($accessToken, $userId, 'calendar/v3/colors');
-		if (!isset($colors['error']) && isset($colors['event'])) {
-			$eventColors = $colors['event'];
-		}
-
-		date_default_timezone_set('UTC');
-		$utcTimezone = new DateTimeZone('-0000');
-		$events = $this->getCalendarEvents($accessToken, $userId, $calId);
-		$nbAdded = 0;
-		foreach ($events as $e) {
-			$calData = 'BEGIN:VCALENDAR' . "\n"
-				. 'VERSION:2.0' . "\n"
-				. 'PRODID:NextCloud Calendar' . "\n"
-				. 'BEGIN:VEVENT' . "\n";
-
-			$objectUri = $e['id'] . '-' . $e['etag'];
-			$calData .= 'UID:' . $ncCalId . '-' . $objectUri . "\n";
-			if (isset($e['colorId'], $eventColors[$e['colorId']], $eventColors[$e['colorId']]['background'])) {
-				$closestCssColor = $this->getClosestCssColor($eventColors[$e['colorId']]['background']);
-				$calData .= 'COLOR:' . $closestCssColor . "\n";
-			}
-			$calData .= isset($e['summary'])
-				? ('SUMMARY:' . substr(str_replace("\n", '\n', $e['summary']), 0, 250) . "\n")
-				: ($e['visibility'] ?? '' === 'private'
-					? ('SUMMARY:' . $this->l10n->t('Private event') . "\n")
-					: '');
-			$calData .= isset($e['sequence']) ? ('SEQUENCE:' . $e['sequence'] . "\n") : '';
-			$calData .= isset($e['location'])
-				? ('LOCATION:' . substr(str_replace("\n", '\n', $e['location']), 0, 250) . "\n")
-				: '';
-			$calData .= isset($e['description'])
-				? ('DESCRIPTION:' . substr(str_replace("\n", '\n', $e['description']), 0, 250) . "\n")
-				: '';
-			$calData .= isset($e['status']) ? ('STATUS:' . strtoupper(str_replace("\n", '\n', $e['status'])) . "\n") : '';
-
-			if (isset($e['created'])) {
-				$created = new Datetime($e['created']);
-				$created->setTimezone($utcTimezone);
-				$calData .= 'CREATED:' . $created->format('Ymd\THis\Z') . "\n";
+		try {
+			$params = [];
+			if ($color) {
+				$params['{http://apple.com/ns/ical/}calendar-color'] = $color;
 			}
 
-			if (isset($e['updated'])) {
-				$updated = new Datetime($e['updated']);
-				$updated->setTimezone($utcTimezone);
-				$calData .= 'LAST-MODIFIED:' . $updated->format('Ymd\THis\Z') . "\n";
+			$newCalName = trim($calName) . ' (' . $this->l10n->t('Google Calendar import') .')';
+			$ncCalId = $this->calendarExists($userId, $newCalName);
+			if (is_null($ncCalId)) {
+				$ncCalId = $this->caldavBackend->createCalendar('principals/users/' . $userId, $newCalName, $params);
 			}
 
-			if (isset($e['reminders'], $e['reminders']['useDefault']) && $e['reminders']['useDefault']) {
-				// 15 min before, default alarm
-				$calData .= 'BEGIN:VALARM' . "\n"
-					. 'ACTION:DISPLAY' . "\n"
-					. 'TRIGGER;RELATED=START:-PT15M' . "\n"
-					. 'END:VALARM' . "\n";
+			// Delete old calendar events
+			// Only trust new ones
+			// They could have been deleted in Google Calendar or moved,
+			// in which case they would be doubled
+			// TODO: This is not very efficient, really we should check what's in Google
+			// Calendar and only delete the stuff that was deleted there
+			foreach ($this->caldavBackend->getCalendarObjects($ncCalId) as $e) {
+				$this->caldavBackend->deleteCalendarObject($ncCalId, $e['uri'], $this->caldavBackend::CALENDAR_TYPE_CALENDAR, true);
 			}
-			if (isset($e['reminders'], $e['reminders']['overrides'])) {
-				foreach ($e['reminders']['overrides'] as $o) {
-					$nbMin = 0;
-					if (isset($o['minutes'])) {
-						$nbMin += (int) $o['minutes'];
-					}
-					if (isset($o['hours'])) {
-						$nbMin += ((int) $o['hours']) * 60;
-					}
-					if (isset($o['days'])) {
-						$nbMin += ((int) $o['days']) * 60 * 24;
-					}
-					if (isset($o['weeks'])) {
-						$nbMin += ((int) $o['weeks']) * 60 * 24 * 7;
-					}
+
+			// get color list
+			$eventColors = [];
+			$colors = $this->googleApiService->request($accessToken, $userId, 'calendar/v3/colors');
+			if (!isset($colors['error']) && isset($colors['event'])) {
+				$eventColors = $colors['event'];
+			}
+
+			date_default_timezone_set('UTC');
+			$utcTimezone = new DateTimeZone('-0000');
+			$events = $this->getCalendarEvents($accessToken, $userId, $calId);
+			$nbAdded = 0;
+			foreach ($events as $e) {
+				$calData = 'BEGIN:VCALENDAR' . "\n"
+					. 'VERSION:2.0' . "\n"
+					. 'PRODID:NextCloud Calendar' . "\n"
+					. 'BEGIN:VEVENT' . "\n";
+
+				$objectUri = $e['id'] . '-' . $e['etag'];
+				$calData .= 'UID:' . $ncCalId . '-' . $objectUri . "\n";
+				if (isset($e['colorId'], $eventColors[$e['colorId']], $eventColors[$e['colorId']]['background'])) {
+					$closestCssColor = $this->getClosestCssColor($eventColors[$e['colorId']]['background']);
+					$calData .= 'COLOR:' . $closestCssColor . "\n";
+				}
+				$calData .= isset($e['summary'])
+					? ('SUMMARY:' . substr(str_replace("\n", '\n', $e['summary']), 0, 250) . "\n")
+					: ($e['visibility'] ?? '' === 'private'
+						? ('SUMMARY:' . $this->l10n->t('Private event') . "\n")
+						: '');
+				$calData .= isset($e['sequence']) ? ('SEQUENCE:' . $e['sequence'] . "\n") : '';
+				$calData .= isset($e['location'])
+					? ('LOCATION:' . substr(str_replace("\n", '\n', $e['location']), 0, 250) . "\n")
+					: '';
+				$calData .= isset($e['description'])
+					? ('DESCRIPTION:' . substr(str_replace("\n", '\n', $e['description']), 0, 250) . "\n")
+					: '';
+				$calData .= isset($e['status']) ? ('STATUS:' . strtoupper(str_replace("\n", '\n', $e['status'])) . "\n") : '';
+
+				if (isset($e['created'])) {
+					$created = new Datetime($e['created']);
+					$created->setTimezone($utcTimezone);
+					$calData .= 'CREATED:' . $created->format('Ymd\THis\Z') . "\n";
+				}
+
+				if (isset($e['updated'])) {
+					$updated = new Datetime($e['updated']);
+					$updated->setTimezone($utcTimezone);
+					$calData .= 'LAST-MODIFIED:' . $updated->format('Ymd\THis\Z') . "\n";
+				}
+
+				if (isset($e['reminders'], $e['reminders']['useDefault']) && $e['reminders']['useDefault']) {
+					// 15 min before, default alarm
 					$calData .= 'BEGIN:VALARM' . "\n"
 						. 'ACTION:DISPLAY' . "\n"
-						. 'TRIGGER;RELATED=START:-PT' . $nbMin . 'M' . "\n"
+						. 'TRIGGER;RELATED=START:-PT15M' . "\n"
 						. 'END:VALARM' . "\n";
 				}
-			}
-
-			if (isset($e['recurrence']) && is_array($e['recurrence'])) {
-				foreach ($e['recurrence'] as $r) {
-					$calData .= $r . "\n";
+				if (isset($e['reminders'], $e['reminders']['overrides'])) {
+					foreach ($e['reminders']['overrides'] as $o) {
+						$nbMin = 0;
+						if (isset($o['minutes'])) {
+							$nbMin += (int) $o['minutes'];
+						}
+						if (isset($o['hours'])) {
+							$nbMin += ((int) $o['hours']) * 60;
+						}
+						if (isset($o['days'])) {
+							$nbMin += ((int) $o['days']) * 60 * 24;
+						}
+						if (isset($o['weeks'])) {
+							$nbMin += ((int) $o['weeks']) * 60 * 24 * 7;
+						}
+						$calData .= 'BEGIN:VALARM' . "\n"
+							. 'ACTION:DISPLAY' . "\n"
+							. 'TRIGGER;RELATED=START:-PT' . $nbMin . 'M' . "\n"
+							. 'END:VALARM' . "\n";
+					}
 				}
-			}
 
-			if (isset($e['start'], $e['start']['date'], $e['end'], $e['end']['date'])) {
-				// whole days
-				$start = new Datetime($e['start']['date']);
-				$calData .= 'DTSTART;VALUE=DATE:' . $start->format('Ymd') . "\n";
-				$end = new Datetime($e['end']['date']);
-				$calData .= 'DTEND;VALUE=DATE:' . $end->format('Ymd') . "\n";
-			} elseif (isset($e['start']['dateTime']) && isset($e['end']['dateTime'])) {
-				$start = new Datetime($e['start']['dateTime']);
-				$start->setTimezone($utcTimezone);
-				$calData .= 'DTSTART;VALUE=DATE-TIME:' . $start->format('Ymd\THis\Z') . "\n";
-				$end = new Datetime($e['end']['dateTime']);
-				$end->setTimezone($utcTimezone);
-				$calData .= 'DTEND;VALUE=DATE-TIME:' . $end->format('Ymd\THis\Z') . "\n";
-			} else {
-				// skip entries without any date
-				continue;
-			}
+				if (isset($e['recurrence']) && is_array($e['recurrence'])) {
+					foreach ($e['recurrence'] as $r) {
+						$calData .= $r . "\n";
+					}
+				}
 
-			$calData .= 'CLASS:PUBLIC' . "\n"
-				. 'END:VEVENT' . "\n"
-				. 'END:VCALENDAR';
-
-			try {
-				$this->caldavBackend->createCalendarObject($ncCalId, $objectUri, $calData);
-				$nbAdded++;
-			} catch (BadRequest $ex) {
-				if (strpos($ex->getMessage(), 'uid already exists') !== false) {
-					$this->logger->debug('Skip existing event', ['app' => $this->appName]);
+				if (isset($e['start'], $e['start']['date'], $e['end'], $e['end']['date'])) {
+					// whole days
+					$start = new Datetime($e['start']['date']);
+					$calData .= 'DTSTART;VALUE=DATE:' . $start->format('Ymd') . "\n";
+					$end = new Datetime($e['end']['date']);
+					$calData .= 'DTEND;VALUE=DATE:' . $end->format('Ymd') . "\n";
+				} elseif (isset($e['start']['dateTime']) && isset($e['end']['dateTime'])) {
+					$start = new Datetime($e['start']['dateTime']);
+					$start->setTimezone($utcTimezone);
+					$calData .= 'DTSTART;VALUE=DATE-TIME:' . $start->format('Ymd\THis\Z') . "\n";
+					$end = new Datetime($e['end']['dateTime']);
+					$end->setTimezone($utcTimezone);
+					$calData .= 'DTEND;VALUE=DATE-TIME:' . $end->format('Ymd\THis\Z') . "\n";
 				} else {
+					// skip entries without any date
+					continue;
+				}
+
+				$calData .= 'CLASS:PUBLIC' . "\n"
+					. 'END:VEVENT' . "\n"
+					. 'END:VCALENDAR';
+
+				try {
+					$this->caldavBackend->createCalendarObject($ncCalId, $objectUri, $calData);
+					$nbAdded++;
+				} catch (BadRequest $ex) {
+					if (strpos($ex->getMessage(), 'uid already exists') !== false) {
+						$this->logger->debug('Skip existing event', ['app' => $this->appName]);
+					} else {
+						$this->logger->warning('Error when creating calendar event "' . '<redacted>' . '" ' . $ex->getMessage(), ['app' => $this->appName]);
+					}
+				} catch (Exception | Throwable $ex) {
 					$this->logger->warning('Error when creating calendar event "' . '<redacted>' . '" ' . $ex->getMessage(), ['app' => $this->appName]);
 				}
-			} catch (Exception | Throwable $ex) {
-				$this->logger->warning('Error when creating calendar event "' . '<redacted>' . '" ' . $ex->getMessage(), ['app' => $this->appName]);
+			}
+
+			$eventGeneratorReturn = $events->getReturn();
+			if (isset($eventGeneratorReturn['error'])) {
+				return $eventGeneratorReturn;
+			}
+			return [
+				'nbAdded' => $nbAdded,
+				'calName' => $newCalName,
+			];
+		} finally {
+			try {
+				unlink($lockFile);
+			} catch (Exception) {
 			}
 		}
-
-		$eventGeneratorReturn = $events->getReturn();
-		if (isset($eventGeneratorReturn['error'])) {
-			return $eventGeneratorReturn;
-		}
-		return [
-			'nbAdded' => $nbAdded,
-			'calName' => $newCalName,
-		];
 	}
 
 	/**
